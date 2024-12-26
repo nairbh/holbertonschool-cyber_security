587081e3bd0c0c717fa5be702533d9feb464b3e6
nairbh <houcinenair@proton.me>
2024-12-26 08:31:42 -0500

    Monitor root activity

--- cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
@@ -6,9 +6,13 @@ if [ "$#" -ne 1 ]; then
 
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <username>"
+    exit 1#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <username>"
     exit 1
 fi
 
 USER=$1
 
-ps -u "$USER" -o user,pid,%cpu,%mem,vsz,rss,command | awk '$5 > 0 && $6 > 0' | head -n 2
+ps -u "$USER" -o user,pid,%cpu,%mem,vsz,rss,command | awk '$5 > 0 && $6 > 0' | head -n 2 | awk '{$1=$1}1'
