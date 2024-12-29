51a8ffd49dbe5ee0bacd5381e8a9f4993fdf60a6
nairbh <houcinenair@proton.me>
2024-12-29 12:42:57 -0500

    push first task and reamde

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F': ' 'BEGIN { print "Category, Field, Value" } $0 ~ /(Registrant|Admin|Tech) / {split($0, arr, ": "); print arr[1] ", " arr[2]}' > "$1.csv"
+whois "$1" | awk -F': ' 'BEGIN { print "Category, Field, Value" } $0 ~ /^(Registrant|Admin|Tech)/ {gsub(/^ | $/, "", $0); gsub(": ", ",", $0); print $0}' > "$1.csv"
