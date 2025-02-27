627fc1ed3ff4b889ec00eccf4c2ca7344c331973
nairbh <houcinenair@proton.me>
2025-02-27 10:08:56 -0500

    update rules

--- linux_security/0x03_protocols_servers/9-firewall.sh
+++ linux_security/0x03_protocols_servers/9-firewall.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 iptables -P INPUT DROP
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT && iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
