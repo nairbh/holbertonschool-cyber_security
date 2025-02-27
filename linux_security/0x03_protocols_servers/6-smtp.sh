a924fa15234a5b38fa1ecaf73e077bdd87eb4ccf
nairbh <houcinenair@proton.me>
2025-02-27 09:58:32 -0500

    update script

--- linux_security/0x03_protocols_servers/6-smtp.sh
+++ linux_security/0x03_protocols_servers/6-smtp.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-CONF="/etc/postfix/main.cf"
-grep -q "smtpd_tls_security_level" "$CONF" && grep "smtpd_tls_security_level" "$CONF" || echo "STARTTLS not configured"
+CONF="/etc/postfix/main.cf" grep -q "smtpd_tls_security_level" "$CONF" && grep "smtpd_tls_security_level" "$CONF" || echo "STARTTLS not configured"
