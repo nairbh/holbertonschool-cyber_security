8e9ec01048c83bcbdbe09f48f0198c63a65dbee6
Houcine <113390396+nairbh@users.noreply.github.com>
2025-12-07 20:28:44 +0100

    Update 2-count_attack.sh

--- 0x0b_web_application_fast_incident_response/2-count_attack.sh
+++ 0x0b_web_application_fast_incident_response/2-count_attack.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-tail -n 1000 auth.log > 1000.log
-log_file=1000.log
-failed_passwords=$(grep 'Failed password' "$log_file")
-accepted_passwords=$(grep 'Accepted password' "$log_file")
-failed_users=$(echo "$failed_passwords" | awk '{print $9}' | sort | uniq)
-accepted_users=$(echo "$accepted_passwords" | awk '{print $9}' | sort | uniq)
-common_users=$(comm -12 <(echo "$failed_users") <(echo "$accepted_users"))
+logs=$(tail -n 1000 /var/log/auth.log)
 
-echo "$common_users"
+compromised=$(echo "$logs" | awk '
+    /Failed password/ { fail[$NF]++ }
+    /Accepted password/ {
+        user=$NF
+        if (fail[user] > 2) {
+            print user
+        }
+    }
+' | sort -u)
+
+echo "$compromised"
