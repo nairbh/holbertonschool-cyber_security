67faca385fb32e7230d42af035f50bd767b144ac
nairbh <houcinenair@proton.me>
2026-04-01 21:36:40 +0200

    add condition

--- linux_security/0x04_buffer_overflow/read_write_heap.py
+++ linux_security/0x04_buffer_overflow/read_write_heap.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
+"""Module that finds and replaces a string in the heap of a running process."""
 import sys
-import os
-"""Documentation :
-Module docs.
-"""
 
-
-pid_, s, r = int(sys.argv[1]), sys.argv[2].encode(), sys.argv[3].encode()
 
 if len(sys.argv) != 4:
     print("Usage: read_write_heap.py pid search_string replace_string")
     sys.exit(1)
+
+pid_, s, r = int(sys.argv[1]), sys.argv[2].encode(), sys.argv[3].encode()
+
 
 heap_over = next(l for l in open(f"/proc/{pid_}/maps") if "heap" in l and "rw" in l)
 
@@ -19,5 +17,4 @@ start, end = (int(x, 16) for x in heap_over.split()[0].split('-'))
 f = open(f"/proc/{pid_}/mem", "r+b")
 
 f.seek(start); data = f.read(end - start)
-
 f.seek(start); f.write(data.replace(s, r, 1))
