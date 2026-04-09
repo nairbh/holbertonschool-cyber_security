1b2de469c89a4dd16b30cabd5d383f253b2dbe7e
nairbh <houcinenair@proton.me>
2026-04-09 21:14:13 +0200

    compte ip address

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -i -E "from" auth.log | awk '{print $12}' | sort | uniq -c | wc -l
+grep -i -E "from|for|Received disconnect from IP" auth.log | awk '{print $12}' | sort | uniq -c | wc -l
