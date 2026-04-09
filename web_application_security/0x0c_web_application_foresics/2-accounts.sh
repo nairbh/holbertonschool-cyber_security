f5eeec48af325f41c0b36f5781538a562a662c04
nairbh <houcinenair@proton.me>
2026-04-09 20:47:22 +0200

    update script

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-tail -1000 auth.log | grep -i -E "failure|accepted" | awk '{print $9}' |sort |uniq -c | head -1 | awk '{print $2}'
+
+tail -1000 auth.log | grep -E "(Failed|Accepted) password" | awk '{print $9}'  |sort |uniq -c | head -1 | awk '{print $2}'
