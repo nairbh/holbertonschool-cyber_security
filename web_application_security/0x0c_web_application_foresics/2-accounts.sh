817e6d0467de21529e417b768fc7c4f57f257bcf
nairbh <houcinenair@proton.me>
2026-04-09 20:33:49 +0200

    update script to read only tail

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -i -E "failure" auth.log | awk '{print $9}' |sort |uniq -c | head -1 | awk '{print $2}'
+tail -1000 auth.log | grep -i -E "failure|accepted" | awk '{print $9}' |sort |uniq -c | head -1 | awk '{print $2}'
